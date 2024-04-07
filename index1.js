240cc503519ebb911455067704c310de549dfee2
punam2003 <punamdeore1353@gmail.com>
2024-04-07 18:18:01 +0530

    github_API

--- index1.js
+++ index1.js
@@ -1,3 +1,20 @@
+export const getGithubDetails = async(req, res) => {
+    const {username} = req.params;
+   const API = `https://api.github.com/users/${username}`
+   
+   axios.get(API)
+    .then((response)=>{
+        res.json(response.data);
+    })
+    .catch((err)=>{
+        console.log("Error occured while fetching github details: ", err)
+
+        res.status(400).json({
+            message: "Error fetching github details",
+            success: false
+        });
+    })  
+}
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
 
@@ -10,3 +27,5 @@ app.use('/api', userRoutes)
 app.listen(3000, ()=>{
     console.log("Server Listening on Port 3000")
 })
+
+
